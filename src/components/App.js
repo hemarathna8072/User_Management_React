5205d8777f007016183fc3313de9eb3e6e831c98
hemarathna8072 <hemarathna721@gmail.com>
2025-03-25 11:37:03 +0530

    basic setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
